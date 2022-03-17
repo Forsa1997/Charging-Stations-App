6714378b159c4a90bfc41c4f49e525c67ee3f3a8
Nicolas Weltzel <nwelt13@gmail.com>
2022-03-17 11:18:30 +0100

    Errors fixed for real?

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -11,8 +11,8 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import { Link as RouterLink, useNavigate } from 'react-router-dom';
-import { Router } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 
 const pages = ['Map'];
 const settings = ['Profile', 'Account', 'Logout'];
